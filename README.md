1ceef321f6f811d465694af2ef0c4ced76fb9bc5
Alessandro Sales <contato@alessandrosales.com.br>
2019-01-20 17:09:17 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# curso-udemy001-backend
+Bem-vindo ao Backend. Esta é apenas uma aplicação de testes que será consumida pelos scripts desenvolvidos com Nightmare.js
+
+Após realizar o clone da aplicação você deve acessar a pasta do projeto pelo terminal e executar os seguintes comandos:
+
+### 1 - Instalação das dependências
+
+`npm install` 
+
+### 2 - Executar o projeto
+
+`DEBUG=backend:* npm start`
+
+Para acessar a aplicação acesse a url: http://localhost:3000 (porta informada pelo terminal)
---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var faker = require('faker');
+faker.locale = "pt_BR";
+
+const photos = [
+	'aventura.jpg',
+	'gato.jpg',
+	'metalica.jpg',
+	'natureza.jpg',
+	'simpsons.jpg',
+	'tecnologia.jpg'
+];
+
+/* Rota para a página inicial */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Node.js e Nightmare.js Automações em páginas Web' });
+});
+
+/* Rota para a página de posts */
+router.get('/posts', function(req, res, next) {
+
+  let posts = [];
+
+  for(let p = 1;p < 10;p++) {
+  	let img = photos[Math.floor(Math.random() * 6)];
+
+  	posts.push({
+  		title: faker.lorem.sentences(1),
+  		content: faker.lorem.paragraph(),
+  		author: faker.name.firstName(),
+  		image: `/images/${img}`
+  	});
+  }
+
+  res.render('posts', { title: 'Postagens', posts: posts });
+});
+
+/* Rota para o formulário */
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Formulário' });
+});
+
+/* Rota para a página de resultado */
+router.get('/result', function(req, res, next) {
+
+  let name = req.query.name || '';
+  let email = req.query.email || '';
+
+  res.render('result', { title: 'Resultado', name, email });
+});
+
+module.exports = router;
